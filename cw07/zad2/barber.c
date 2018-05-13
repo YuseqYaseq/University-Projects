7410916f0b0650f6f82410ec9eaaaba0d0d253fd
YuseqYaseq <jozefjasek@gmail.com>
2018-05-13 17:57:36 +0200

    Im done.

--- cw07/zad2/barber.c
+++ cw07/zad2/barber.c
@@ -4,94 +4,10 @@
 #include "lib.h"
 #include "queue.h"
 
-#define BARBER_READY ("barber_ready")
-#define ACCESS_SEATS ("access_seats")
-#define CUST_READY ("cust_ready")
+#define QUEUE ("sem1")
+#define BARBER ("sem2")
+#define BARBER_TEST ("sem3")
 
-#define CNAME ("customers")
-#define QNAME ("queue")
-#define QSIZE (sizeof(struct Queue) + sizeof(int))
-
-sem_t* barberReady;
-sem_t* accessWRSeats;
-sem_t* custReady;
-int* numOfCust;
-
-int* numOfFreeWRSeats;
-struct Queue* Data;
-
-void onExit(int t){
-    CloseSemaphore(barberReady);
-    CloseSemaphore(accessWRSeats);
-    CloseSemaphore(custReady);
-    RemoveSemaphore(BARBER_READY);
-    RemoveSemaphore(ACCESS_SEATS);
-    RemoveSemaphore(CUST_READY);
-    ReleaseSharedMem(numOfFreeWRSeats, QSIZE, QNAME);
-    ReleaseSharedMem(numOfCust, sizeof(int), CNAME);
-    exit(0);
-}
-
-void Init(int N){
-    signal(SIGINT, onExit);
-    signal(SIGTERM, onExit);
-    
-    barberReady = CreateSemaphore(BARBER_READY, 0);
-    accessWRSeats = CreateSemaphore(ACCESS_SEATS, 1);
-    custReady = CreateSemaphore(CUST_READY, 1);
-    
-    numOfCust = (int*)CreateSharedMem(CNAME, sizeof(int));
-    numOfFreeWRSeats = (int*)CreateSharedMem(QNAME, QSIZE);
-    
-    *numOfCust = 0;
-    *numOfFreeWRSeats = N;
-    
-    Data = (struct Queue*)(numOfFreeWRSeats+1);
-    QInit(Data, N+1);
-}
-
-int main(int c, char* v[]){
-    if(c < 2){
-        printf("Uzycie programu: ./b N\n");
-        printf("N - liczba siedzen w kolejce do golibrody.\n");
-        return 0;
-    }
-    
-    Init(atoi(v[1]));
-    
-    while(1){
-        for(int i = 0; i < Data->size; ++i){
-                printf("         Q[%d] = %d\n", i, Data->elems[i]);
-            }
-    
-        Take(custReady);
-        if((*numOfCust) == 0){
-            Release(custReady);
-            continue;
-        }
-        (*numOfCust)--;
-        Release(custReady);
-        
-        Take(accessWRSeats);
-        
-        int cust_pid = QGet(Data);
-        (*numOfFreeWRSeats)++;
-        
-        Release(barberReady);
-        Release(accessWRSeats);
-        
-        //Work here
-        //sleep(1);
-        printf("Cutting %d.\n", cust_pid);
-        
-        
-        kill(cust_pid, SIGUSR1);
-        
-    }
-    onExit(0);
-}
-
-/*
 sem_t* SemQ;
 sem_t* SemB;
 sem_t* SemT;
@@ -141,10 +57,6 @@ int main(int c, char* v[]){
             printf("Waking up.\n");
         }
         Take(SemQ);
-            printf("Q: in = %d, out = %d, size = %d.\n", Data->queueIn, Data->queueOut, Data->size);
-            for(int i = 0; i < Data->size; ++i){
-                printf("         Q[%d] = %d\n", i, Data->elems[i]);
-            }
             int pid = QGet(Data);
         Release(SemQ);
         
@@ -159,8 +71,6 @@ int main(int c, char* v[]){
     
     onExit(0);
 }
-
-*/
 
 
 
--- cw07/zad2/client.c
+++ cw07/zad2/client.c
@@ -4,106 +4,34 @@
 #include "lib.h"
 #include "queue.h"
 
-#define BARBER_READY ("barber_ready")
-#define ACCESS_SEATS ("access_seats")
-#define CUST_READY ("cust_ready")
+#define QUEUE ("sem1")
+#define BARBER ("sem2")
+#define BARBER_TEST ("sem3")
 
-#define CNAME ("customers")
-#define QNAME ("queue")
-#define QSIZE (sizeof(struct Queue) + sizeof(int))
-
-sem_t* barberReady;
-sem_t* accessWRSeats;
-sem_t* custReady;
-int* numOfCust;
-
-int* numOfFreeWRSeats;
+sem_t* SemQ;
+sem_t* SemB;
+sem_t* SemT;
 struct Queue* Data;
+size_t N;
 
-int N;
+int over;
+int used;
+int pid;
 int s;
 
-int cut;
-
 void onExit(int t){
-    CloseSemaphore(barberReady);
-    CloseSemaphore(accessWRSeats);
-    CloseSemaphore(custReady);
-    CloseSharedMem(numOfFreeWRSeats, QSIZE);
-    CloseSharedMem(numOfCust, sizeof(int));
+    CloseSemaphore(SemQ);
+    CloseSemaphore(SemB);
+    CloseSemaphore(SemT);
+    CloseSharedMem(Data, sizeof(struct Queue));
     exit(0);
 }
 
-void finished(int t){
-    printf("%d: Hair cut!\n", getpid());
-    cut = 1;
-}
 
-void Init(){
-    signal(SIGUSR1, finished);
-    
-    barberReady = GetSemaphore(BARBER_READY);
-    accessWRSeats = GetSemaphore(ACCESS_SEATS);
-    custReady = GetSemaphore(CUST_READY);
-    
-    numOfFreeWRSeats = (int*)GetSharedMem(QNAME, QSIZE);
-    Data = (struct Queue*)(numOfFreeWRSeats+1);
-    
-    numOfCust = (int*)GetSharedMem(CNAME, sizeof(int));
-}
-
-int main(int c, char* v[]){
-    if(c < 3){
-        printf("Uzycie programu: ./c N S\n");
-        printf("N - liczba klientow do stworzenia.\n");
-        printf("S - liczba strzyzen dla klienta.\n");
-        return 0;
-    }
-    N = atoi(v[1]);
-    s = atoi(v[2]);
-    
-    Init();
-    
-    for(int i = 0; i < N; ++i){
-        if(fork() == 0){
-            cut = 0;
-            Take(accessWRSeats);
-            printf("%d: %d wolnych miejsc.\n", getpid(), (*numOfFreeWRSeats));
-            if((*numOfFreeWRSeats) > 0){
-                
-                Take(custReady);
-                
-                (*numOfCust)++;
-                (*numOfFreeWRSeats)--;
-                QPut(Data, getpid());
-                
-                printf("   l\n");
-                Release(custReady);
-                Release(accessWRSeats);
-                
-                Take(barberReady);
-                while(cut == 0);
-            }else{
-                //no free seats
-                Release(accessWRSeats);
-                printf("No free seats!\n");
-            }
-            
-            break;
-        }
-    }
-    onExit(0);
-}
-
-
-/*
 void goToB(){
     used = 0;
-    printf("%d: Take(SemQ = %d)\n", pid, GetValue(SemQ));
     Take(SemQ);
-        printf("   %d: Entering CRIT 1\n", pid);
         int r = QPut(Data, pid);
-        printf("   %d: Leaving CRIT 1\n", pid);
     Release(SemQ);
     
     printf("%d: Entering queue.\n", pid);
@@ -111,9 +39,7 @@ void goToB(){
         printf("%d: Queue full! Leaving the barber's.\n", pid);
         onExit(0);
     }else{
-        printf("%d: Take(SemT = %d)\n", getpid(), GetValue(SemT));
         Take(SemT);
-            printf("   %d: Entering CRIT 2\n", pid);
             printf("%d: Wake up the barber? SemB = %d\n", getpid(), GetValue(SemB));
             if(GetValue(SemB) == 0){
                 Release(SemB);
@@ -121,7 +47,6 @@ void goToB(){
                 Release(SemB);
                 printf("%d: Waking up the barber.\n", pid);
             }
-            printf("   %d: Leaving CRIT 2\n", pid);
         Release(SemT);
     }
     while(used == 0);
@@ -151,7 +76,7 @@ int main(int c, char* v[]){
     
     SemQ = GetSemaphore(QUEUE);
     SemB = GetSemaphore(BARBER);
-    SemT = /*CreateSemaphore(BARBER_TEST, 0);*//*GetSemaphore(BARBER_TEST);
+    SemT = /*CreateSemaphore(BARBER_TEST, 0);*/GetSemaphore(BARBER_TEST);
     
     Data = (struct Queue*)GetSharedMem(QUEUE, sizeof(struct Queue));
     int N = atoi(v[1]);
@@ -168,8 +93,6 @@ int main(int c, char* v[]){
     
     onExit(0);
 }
-
-*/
 
 
 
