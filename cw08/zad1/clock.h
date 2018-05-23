8991c8d2bcce13bceb777f976281ad466d8b7a6c
YuseqYaseq <jozefjasek@gmail.com>
2018-05-23 20:29:28 +0200

    Added ex08. Currently only on one thread.

---
+++ cw08/zad1/clock.h
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/times.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+
+
+
+clock_t start;
+clock_t finish;
+struct tms tmsstart;
+struct tms tmsfinish;
+
+void startClock() {
+    start = times(&tmsstart);
+}
+
+void finishClock() {
+    long ticks = sysconf(_SC_CLK_TCK);
+    finish = times(&tmsfinish);
+    printf("Czas rzeczywisty: %f\nCzas usera: %f\nCzas systemowy: %f\n",
+		(finish - start) / (double)ticks,
+		(tmsfinish.tms_utime - tmsstart.tms_utime) / (double)ticks,
+		(tmsfinish.tms_stime - tmsstart.tms_stime) / (double)ticks);
+}
+
+
+bool isNumber(char* string){
+    int n;
+    int value;
+    char c;
+    n = sscanf(string, "%d%c", &value, &c);
+    if(n != 1) return false;
+    return true;
+}
+
+//brzydkie a szybkie
+int digits(int n){
+    if(n < 10)          return 1;
+    if(n < 100)         return 2;
+    if(n < 1000)        return 3;
+    if(n < 10000)       return 4;
+    if(n < 100000)      return 5;
+    if(n < 1000000)     return 6;
+    if(n < 10000000)    return 7;
+    if(n < 100000000)   return 8;
+    if(n < 1000000000)  return 9;
+    return 10;
+}
