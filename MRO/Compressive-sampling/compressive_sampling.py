437d1d1f7f61fc8ba720764a02c977a571c915f4
YuseqYaseq <jozefjasek@gmail.com>
2019-11-14 22:13:10 +0100

    Added DCT

---
+++ MRO/Compressive-sampling/compressive_sampling.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import scipy.optimize as spopt
+import scipy.fftpack as spfft
+import scipy.ndimage as spimg
+import cvxpy as cvx
+import cv2
+
+
+def dct2(x):
+    return spfft.dct(spfft.dct(x.T, norm='ortho', axis=0).T, norm='ortho', axis=0)
+
+
+def idct2(x):
+    return spfft.idct(spfft.idct(x.T, norm='ortho', axis=0).T, norm='ortho', axis=0)
+
+
+def create_masks(nx, ny):
+    masks = []
+    first_10_10_1 = np.zeros((nx, ny))
+    first_10_10_1[:10, :10] = 1
+
+    first_50_50_1 = np.zeros((nx, ny))
+    first_50_50_1[:50, :50] = 1
+
+    first_100_100_1 = np.zeros((nx, ny))
+    first_100_100_1[:100, :100] = 1
+
+    first_10_10_0 = np.ones((nx, ny))
+    first_10_10_0[:10, :10] = 0
+
+    first_50_50_0 = np.ones((nx, ny))
+    first_50_50_0[:50, :50] = 0
+
+    first_100_100_0 = np.ones((nx, ny))
+    first_100_100_0[:100, :100] = 0
+
+    proportionally_to_position = np.zeros((nx, ny))
+    for i in range(nx):
+        for j in range(ny):
+            proportionally_to_position[i, j] = max(1 - 20 * (i+j) / (nx + ny), 0)
+
+    removed_shape = np.ones((nx, ny))
+    removed_shape[5:15, 5:15] = 0
+
+    removed_shape_2 = np.ones((nx, ny))
+    removed_shape_2[5:30, 5:30] = 0
+
+    masks.append(("Pierwsze 10x10 częstotliwości = 1",first_10_10_1))
+    masks.append(("Pierwsze 50x50 częstotliwości = 1", first_50_50_1))
+    masks.append(("Pierwsze 100x100 częstotliwości = 1", first_100_100_1))
+    masks.append(("Pierwsze 10x10 częstotliwości = 0", first_10_10_0))
+    masks.append(("Pierwsze 50x50 częstotliwości = 0", first_50_50_0))
+    masks.append(("Pierwsze 100x100 częstotliwości = 0", first_100_100_0))
+    masks.append(("Proporcjonalnie do pozycji na obrazie", proportionally_to_position))
+    masks.append(("Usunięty kwadrat 5x15, 5x15", removed_shape))
+    masks.append(("Usunięty kwadrat 5x30, 5x30", removed_shape_2))
+
+    return masks
+
+
+def remove_n_pixels(image: np.ndarray, no_of_zeros: int):
+    selected_pixels = np.array([0] * no_of_zeros + [1] * (image.size - no_of_zeros))
+    np.random.shuffle(selected_pixels)
+    selected_pixels = selected_pixels.reshape(image.shape)
+    image = np.multiply(image, selected_pixels)
+    return image
+
+
+def calc(x, mask):
+    freq = dct2(x)
+    freq = np.multiply(freq, mask)
+    result = idct2(freq)
+    return result
+
+
+def main():
+    nx = 1000
+    ny = 1000
+    imgs = ['a.jpg', 'b.jpg', 'c.jpg']
+    masks = create_masks(nx, ny)
+
+    # HW part 1
+    for i, img in enumerate(imgs):
+        x = cv2.imread(img)  # read img
+        x = cv2.resize(x, (nx, ny))  # scale down
+        x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)  # to grayscale
+        plt.imshow(x)
+        plt.title("Oryginalny obraz")
+        plt.savefig("imgs//orig" + str(i))
+
+        # HW part 1
+        '''for title, mask in masks:
+            result = calc(x, mask)
+            plt.imshow(result)
+            plt.title(title)
+            plt.savefig("imgs//" + title + str(i))'''
+
+        # HW part 2
+        filtered = remove_n_pixels(x, 900_000)
+        extrapolated = idct2(dct2(filtered))
+        plt.imshow(extrapolated)
+        plt.show()
+
+
+main()
