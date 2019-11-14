a9d74004ea3565cf90e4606df8ad90d6e7d8e46e
YuseqYaseq <jozefjasek@gmail.com>
2019-11-14 23:22:21 +0100

    Added part 2.

--- MRO/Compressive-sampling/compressive_sampling.py
+++ MRO/Compressive-sampling/compressive_sampling.py
@@ -6,6 +6,7 @@ import scipy.fftpack as spfft
 import scipy.ndimage as spimg
 import cvxpy as cvx
 import cv2
+from pylbfgs import owlqn
 
 
 def dct2(x):
@@ -75,33 +76,94 @@ def calc(x, mask):
     return result
 
 
-def main():
-    nx = 1000
-    ny = 1000
-    imgs = ['a.jpg', 'b.jpg', 'c.jpg']
-    masks = create_masks(nx, ny)
+def evaluate(x, g, step, nx=1000, ny=1000, ):
+    """An in-memory evaluation callback."""
+
+    # we want to return two things: 
+    # (1) the norm squared of the residuals, sum((Ax-b).^2), and
+    # (2) the gradient 2*A'(Ax-b)
+
+    # expand x columns-first
+    print(x.shape)
+    x2 = x.reshape((nx, ny)).T
+
+    # Ax is just the inverse 2D dct of x2
+    Ax2 = idct2(x2)
+
+    # stack columns and extract samples
+    Ax = Ax2.T.flat[ri].reshape(b.shape)
+
+    # calculate the residual Ax-b and its 2-norm squared
+    Axb = Ax - b
+    fx = np.sum(np.power(Axb, 2))
+
+    # project residual vector (k x 1) onto blank image (ny x nx)
+    Axb2 = np.zeros(x2.shape)
+    Axb2.T.flat[ri] = Axb # fill columns-first
+
+    # A'(Ax-b) is just the 2D dct of Axb2
+    AtAxb2 = 2 * dct2(Axb2)
+    AtAxb = AtAxb2.T.reshape(x.shape) # stack columns
+
+    # copy over the gradient vector
+    np.copyto(g, AtAxb)
+
+    return fx
+
+
+nx = 1000
+ny = 1000
+imgs = ['a.jpg', 'b.jpg', 'c.jpg']
+masks = create_masks(nx, ny)
+
+
+for i, img in enumerate(imgs):
+    x = cv2.imread(img)  # read img
+    x = cv2.resize(x, (nx, ny))  # scale down
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)  # to grayscale
+    plt.imshow(x)
+    plt.title("Oryginalny obraz")
+    plt.savefig("imgs//orig" + str(i))
 
     # HW part 1
-    for i, img in enumerate(imgs):
-        x = cv2.imread(img)  # read img
-        x = cv2.resize(x, (nx, ny))  # scale down
-        x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)  # to grayscale
-        plt.imshow(x)
-        plt.title("Oryginalny obraz")
-        plt.savefig("imgs//orig" + str(i))
+    '''for title, mask in masks:
+        result = calc(x, mask)
+        plt.imshow(result)
+        plt.title(title)
+        plt.savefig("imgs//" + title + str(i))'''
 
-        # HW part 1
-        '''for title, mask in masks:
-            result = calc(x, mask)
-            plt.imshow(result)
-            plt.title(title)
-            plt.savefig("imgs//" + title + str(i))'''
+    # HW part 2
+    
+    # fractions of the scaled image to randomly sample at
+    sample_sizes = (0.1, 0.01)
+    
+    # for each sample size
+    Z = [np.zeros(x.shape, dtype='uint8') for s in sample_sizes]
+    masks = [np.zeros(x.shape, dtype='uint8') for s in sample_sizes]
+    for j,s in enumerate(sample_sizes):
 
-        # HW part 2
-        filtered = remove_n_pixels(x, 900_000)
-        extrapolated = idct2(dct2(filtered))
-        plt.imshow(extrapolated)
-        plt.show()
+        # create random sampling index vector
+        k = round(nx * ny * s)
+        ri = np.random.choice(nx * ny, k, replace=False) # random sample of indices
 
+        # create images of mask (for visualization)
+        Xm = 255 * np.ones(x.shape)
+        Xm.T.flat[ri] = x.T.flat[ri]
+        masks[j][:,:] = Xm
 
-main()
+        # take random samples of image, store them in a vector b
+        b = x.T.flat[ri].astype(float)
+
+        # perform the L1 minimization in memory
+        Xat2 = owlqn(nx*ny, evaluate, None, 5)
+
+        # transform the output back into the spatial domain
+        Xat = Xat2.reshape(nx, ny).T # stack columns
+        Xa = idct2(Xat)
+        Z[j][:,:] = Xa.astype('uint8')
+        plt.imshow(Z[j])
+        plt.title("Odtwarzanie obrazu z próbki " + str(s * 100) + "% obrazu")
+        plt.savefig("imgs//Z" + str(j) + str(i))
+        plt.imshow(Xm)
+        plt.title("Maska pozostawiająca " + str(s * 100) + "% obrazu")
+        plt.savefig("imgs//Mask" + str(j) + str(i))
